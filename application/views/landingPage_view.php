d78c626f8c702e2ce7fa3674cff57b5df36d1ab2
Md Zahed Hossain <mdhossain@mdzahedhossain.com>
2014-02-22 13:26:42 -0500

    updating langingPage_view.php file

--- application/views/landingPage_view.php
+++ application/views/landingPage_view.php
@@ -53,7 +53,7 @@
               Einstein
             </div>
           <br/>
-          <form class="form-signin" method="POST" action="../../Einstien/index.php/searchResults/processSearch">
+          <form id="id-form-landing" class="form-signin" method="POST" action="../../Einstien/index.php/searchResults/processSearch">
             <input type="text" class="form-control input-lg search-bar" name="query" id="query" placeholder="Disover and Learn"autofocus=""></input>
               <br/><br/>
               <input type="hidden" id = "long" name="long" value='45.99'/>
@@ -68,15 +68,103 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
     <script type="text/javascript">
+
+
+      /*
+      var availableAutosuggestions = [
+        "Learn how to",
+        "Learn",
+        "Learn to",
+        "near",
+        "around",
+        "by"
+      ];
+
+      $('#query').autocomplete({
+        source : availableAutosuggestions
+
+      });
+      */
 
     // Script that gets the Location and then forwards it to the backend.
 
     //Populate Hiddent Fields within the form, update the query field with the remaing and send to Controller - Leave this to Zahid.
 
     // Only disect out the Location, do the rest in PHP in the Backend.
+
+      // we first grab the search query
+      $('#id-form-landing').submit(function(e){
+          //alert('Handler for submit called');
+          // prevent the default submission, dissect the query, get the latitude and longitude and then finally
+          // submit using jquery post
+          e.preventDefault();
+
+          var searchQuery = $('#query').val();
+          var modifiedQuery = "";
+          var searchLocation = "";
+          var learnTopic = "";
+          var locationFound = false;
+          //alert(searchQuery);
+
+          if(searchQuery == '')
+          {
+            alert('Must enter a search term!');
+            return;
+          }
+          else
+          {
+            //alert(searchQuery);
+            // see if any of near, by or around keyword is in the query
+            var indexNear = 0;
+            var indexAround = 0;
+            var indexBy = 0;
+
+            indexNear = (searchQuery.split("near")).length;
+            indexAround = (searchQuery.split("around")).length;
+            indexBy = (searchQuery.split("by")).length;
+
+            if(indexNear > 1)
+            {
+              // we found near in the query
+              searchLocation = (searchQuery.split("near"))[1];
+              modifiedQuery = (searchQuery.split(" near"))[0];
+              learnTopic = (modifiedQuery.split("how to "))[1];
+              locationFound = true;
+            }
+            else if (indexAround > 1) 
+            {
+              // keyword around is found in query
+              searchLocation = (searchQuery.split("around"))[1];
+              modifiedQuery = (searchQuery.split(" around"))[0];
+              learnTopic = (modifiedQuery.split("how to "))[1];
+              locationFound = true;
+            }
+            else if(indexBy > 1)
+            {
+              searchLocation = (searchQuery.split("by"))[1];
+              modifiedQuery = (searchQuery.split(" by"))[0];
+              learnTopic = (modifiedQuery.split("how to "))[1];
+              locationFound = true;
+            }
+            else
+            {
+              searchLocation = "navigator";
+              locationFound = false;
+              modifiedQuery = searchQuery;
+              learnTopic = (modifiedQuery.split("how to "))[1];
+            }
+
+            alert("learnTopic : " + learnTopic + " at searchLocation: " + searchLocation);
+            //return;
+
+          }  // end else query
+
+      }); // end submit 
+
 
     </script>
 
