923139776ce603cbbbedf658744ce2dd559ddda8
beltex <cse93251@cse.yorku.ca>
2014-02-23 05:11:20 -0500

    More view work

--- application/views/masterProfile_view.php
+++ application/views/masterProfile_view.php
@@ -1,61 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Using this Style Sheet for the Einstien Font - Not sure if it flows with the complete theme - Ask Omeed for suggestions -->
-    <!-- http://designshack.net/articles/css/the-10-best-script-and-handwritten-google-web-fonts/ -->
-    <link href="http://fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet" type="text/css">
+    <?php $this->load->view('head'); ?>
+  
+    <body>
+
+    <?php $this->load->view('navbar'); ?>
     
-    <title>Einstein - Discover and Learn</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="../css/ceeko.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="../../Einstien" id = "header-text">Einstein</a>
-        </div>
-        
-        <!-- Search Bar on the Top Nav-->
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control search-bar-top-nav" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Search</button>
-        </form>
-
-
-          <ul class="nav navbar-nav navbar-right">
-            <?php if($login < 1) 
-              echo "<a href='$login_url' class='btn btn-primary navbar-btn'>Sign in</a>";
-              else { ?> 
-              <img class = "navbar-user-image" src="http://graph.facebook.com/<?php echo $user_profile['id']?>/picture"><div class = "navbar-user-name"><?php echo $user_profile['name']; ?></div>
-            <?php
-              }
-            ?>  
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
     <div class="container">
 
       <div class = "jumbotron" style = "height : 700px;">
@@ -96,6 +47,8 @@
         <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://google.ca" data-via="Einstien" data-lang="en" data-text = "Checkout Joh Doe, He's giving guitar Lessons" data-hashtags = "GuitarLessons" data-related="anywhereTheJavascriptAPI" data-count="vertical">Tweet</a>  
          
     </div> <!-- /container -->
+
+    <?php $this->load->view('footer'); ?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
