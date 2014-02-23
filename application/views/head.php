ecfc2409c0e88e891592cd70a63c7eec78ad3051
beltex <cse93251@cse.yorku.ca>
2014-02-23 02:02:39 -0500

    Minor change to head

--- application/views/head.php
+++ application/views/head.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
-
-    <title>Einstein</title>
+    
+       <?php 
+        <!-- TODO -->
+        if (strcmp(basename(__FILE__), 'searchResults_view.php') == 0) {
+            echo "<title>Einstein // (CORE SEARCH TERM)</title>";
+        } 
+        else {
+            echo "<title>Einstein</title>";
+        }
+       ?> 
 
     <!-- Bootstrap core CSS - MUST COME FIRST -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -14,4 +20,3 @@
     <!-- Custom styles for this template -->
     <link href="css/ceeko.css" rel="stylesheet">
   </head>
-</html>
--- application/views/searchResults_view.php
+++ application/views/searchResults_view.php
@@ -1,20 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Einstein // (CORE SEARCH TERM)</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../../css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="../../css/ceeko.css" rel="stylesheet">
-
-  </head>
-
+  <?php $this->load->view('head'); ?>
+  
   <body>
 
     <!-- Fixed navbar -->
