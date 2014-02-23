bf0c2c62c6574a77f37e3ba2a2e80b4cc47af6ab
beltex <cse93251@cse.yorku.ca>
2014-02-23 04:44:55 -0500

    Bug fixies for includes php logic

--- application/views/searchResults_view.php
+++ application/views/searchResults_view.php
@@ -5,37 +5,8 @@
   
   <body>
 
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"></button>
-          <a class="navbar-brand" href="../../../Einstien" id = "header-text">Einstein</a>
-        </div>
-        
-        <!-- Search Bar on the Top Nav-->
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control search-bar-top-nav" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Search</button>
-        </form>
-
-
-         <ul class="nav navbar-nav navbar-right">
-          <?php if($login < 1) 
-            echo "<a href='$login_url' class='btn btn-primary navbar-btn'>Sign in</a>";
-            else { ?> 
-              <img class = "navbar-user-image" src="http://graph.facebook.com/<?php echo $user_profile['id']?>/picture">
-              <div class = "navbar-user-name"><?php echo $user_profile['name']; ?></div>
-            <?php
-              }
-            ?>  
-        </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
+    <?php $this->load->view('navbar'); ?>
+    
     <div class="container">
 
        <!-- Main component for a primary marketing message or call to action -->
